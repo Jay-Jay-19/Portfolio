950c89703664461828b115498364d1dafc5b412a
gilbert mougamadou <gilbertmougamadou@yahoo.fr>
2024-03-12 14:11:12 +0100

    *complete the navbar design

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -3,27 +3,27 @@
 import Link from "next/link";
 import Image from "next/image";
 import { useState } from "react";
+import NavLink from "./navLink";
 
 const links = [
   { url: "/", title: "Home" },
   { url: "/about", title: "About" },
   { url: "/portfolio", title: "Portfolio" },
   { url: "/contact", title: "Contact" },
-
 ];
 
 const Navbar = () => {
   const [open, setOpen] = useState(false);
   return (
-    <div className="h-full flex items-center justify-between px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48">
+    <div className="h-full flex items-center justify-between px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48 text-xl">
       {/* LINKS */}
       <div className="hidden md:flex gap-4 w-1/3">
-        {links.map(link => (
-          <Link href={link.url} key={link.title}>{link.title}</Link>
+        {links.map((link) => (
+          <NavLink link={link} key={link.title} />
         ))}
       </div>
       {/* LOGO */} 
-      <div className="md:hidden lg:flex w-1/3 justify-center">
+      <div className="md:hidden lg:flex xl:w-1/3 xl:justify-center">
         <Link href="/" className="text-sm bg-black rounded-md p-1 font-semibold flex items-center justify-center">
           <span className="text-white mr-1">Jay</span>
           <span className="w-12 h-8 rounded bg-white text-black flex items-center justify-center">.dev</span>
