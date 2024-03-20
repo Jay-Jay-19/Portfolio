72971f8c908cef2cfe7323ccae09aa7186c4abdd
gilbert mougamadou <gilbertmougamadou@yahoo.fr>
2024-03-20 12:02:07 +0100

    Create Contact page

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,8 +1,15 @@
-"use client"
+"use client";
 
 import { motion } from "framer-motion";
+import { useState } from "react";
 
 const ContactPage = () => {
+
+  const [success, setSuccess] = useState(true);
+  const [error, setError] = useState(true);
+
+  const text = "Say Hello";
+
   return (
     <motion.div
     className="h-full"
@@ -10,7 +17,35 @@ const ContactPage = () => {
     animate={{ y: "0%" }}
     transition={{ duration: 1 }}
     >
-      Contact
+      <div className="h-full flex flex-col lg:flex-row px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48">
+        {/* TEXT CONTAINER */}
+        <div className="h-1/2 lg:h-full lg:w-1/2 flex items-center justify-center text-6xl">
+          <div>
+            {text.split("").map((letter, index) => (
+              <motion.span
+                key={index}
+                initial={{ opacity: 1 }}
+                animate={{ opacity: 0 }}
+                transition={{ duration: 3, repeat: Infinity, delay: index * 0.1, }}
+              >
+                {letter}
+              </motion.span>
+            ))}
+            😊
+          </div>
+        </div>
+        {/* CONTACT FORM CONTAINER */}
+        <form className="h-1/2 lg:h-full lg:w-1/2 bg-red-50 rounded-xl text-xl flex flex-col gap-8 justify-center p-24">
+          <span>Cher Jay,</span>
+          <textarea rows={7} className="bg-transparent border-b-2 border-b-black outline-none resize-none"/>
+          <span>Mon adresse email est :</span>
+          <input type="text" className="bg-transparent border-b-2 border-b-black outline-none" />
+          <span>Cordialement,</span>
+          <button className="bg-purple-200 rounded font-semibold text-gray-600 p-4">Envoyer</button>
+          { success && <span className="text-green-600 font-semibold text-center ">🆗 Votre message a bien été envoyé !</span> }
+          { error && <span className="text-red-600 font-semibold text-center">🤔 Echec de l'envoi, un problème est survenu...</span> }
+        </form>
+      </div>
     </motion.div>
   );
 };
