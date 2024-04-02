996dfff210310b91c328c0c98a10578b4d8a6ab0
gilbert mougamadou <gilbertmougamadou@yahoo.fr>
2024-04-02 15:09:45 +0200

    Edit Contact page - minor responsive changes

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -63,7 +63,7 @@ const ContactPage = () => {
         <form
           onSubmit={sendEmail}
           ref={form}
-          className="h-2/3 md:h-full md:w-1/2 mb-4 bg-red-50 rounded-xl text-xl md:text-sm xl:text-xl flex flex-col gap-8 justify-center p-12 md:p-18 xl:p-24"
+          className="h-2/3 md:h-full md:w-1/2 mb-8 bg-red-50 rounded-xl text-xl md:text-sm xl:text-xl flex flex-col gap-8 justify-center p-12 md:p-18 xl:p-24"
         >
           <span>Cher Jay,</span>
           <textarea
