6b4ed58961fe866ed8421ce092be7830ba536f8a
gilbert mougamadou <gilbertmougamadou@yahoo.fr>
2024-04-02 14:34:47 +0200

    Edit Contact page - adjusting responsiveness

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -43,9 +43,9 @@ const ContactPage = () => {
     animate={{ y: "0%" }}
     transition={{ duration: 1 }}
     >
-      <div className="h-full flex flex-col lg:flex-row px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48">
+      <div className="h-full flex flex-col md:flex-row px-8 md:px-12 lg:px-20 xl:px-48">
         {/* TEXT CONTAINER */}
-        <div className="h-1/2 lg:h-full lg:w-1/2 flex items-center justify-center text-6xl">
+        <div className="h-1/3 md:h-full md:w-1/2 flex items-center justify-center text-6xl">
           <div>
             {text.split("").map((letter, index) => (
               <motion.span
@@ -63,11 +63,11 @@ const ContactPage = () => {
         <form
           onSubmit={sendEmail}
           ref={form}
-          className="h-1/2 lg:h-full lg:w-1/2 bg-red-50 rounded-xl text-xl flex flex-col gap-8 justify-center p-12 lg:p-24"
+          className="h-2/3 md:h-full md:w-1/2 mb-4 bg-red-50 rounded-xl text-xl md:text-sm xl:text-xl flex flex-col gap-8 justify-center p-12 md:p-18 xl:p-24"
         >
           <span>Cher Jay,</span>
           <textarea
-            rows={7}
+            rows={4}
             className="bg-transparent border-b border-b-gray-400 outline-none resize-none"
             name="user_message"
           />
