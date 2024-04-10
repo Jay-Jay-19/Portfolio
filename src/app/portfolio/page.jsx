256bdb9ffab37fce5254912eae47e0c52f1efbe7
gilbert mougamadou <gilbertmougamadou@yahoo.fr>
2024-04-10 16:59:42 +0200

    Edit Portfolio page - Delete import of the video for former html video tag

--- src/app/portfolio/page.jsx
+++ src/app/portfolio/page.jsx
@@ -4,7 +4,6 @@ import { motion, useScroll, useTransform } from "framer-motion";
 import Image from "next/image";
 import Link from "next/link";
 import { useRef } from "react";
-import OrangeEstate from "../../../public/orangeEstate-demo.mp4";
 
 const PortfolioPage = () => {
 
